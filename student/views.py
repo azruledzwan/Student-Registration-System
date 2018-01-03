2ee446d252b80c06f56883b6437fd6925f3467cb
azruledzwan <azrulizwan97@gmail.com>
2018-01-03 11:24:02 +0800

    edit form

--- student/views.py
+++ student/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy
 from django.contrib import messages
 #from django.utils import timezone
 from .models import student
-#from .forms import MessageForm, SearchForm, StudentForm
+from .forms import studentForm
 #from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 from django_datatables_view.base_datatable_view import BaseDatatableView
@@ -20,6 +20,27 @@ def home(request):
 
 def home_json(request):
 	return render(request,'student/home_json.html')
+
+def student_new(request):
+
+    if request.method == "POST":
+        form = studentForm(request.POST)
+        if form.is_valid():
+            student = form.save(commit=False)
+            student.createdby = request.user
+            student.save()
+            # return redirect('post_detail', pk=post.pk)
+            messages.success(request, "student record with ID: " + str(student.pk) + " has been created ! ")
+            #return redirect(reverse_lazy('student_detail',kwargs={'pk': student.pk }))
+    else:
+        form = studentForm()
+    
+    return render(request, 'student/student_new.html', {'form': form})
+
+def student_detail(request,pk):
+    Student = get_object_or_404(student, pk=pk)
+    return render(request, 'student/student_detail.html', {'student': Student})
+
 
 	# Student JSON list filtering
 class student_list_json(BaseDatatableView):
@@ -95,8 +116,8 @@ class student_list_json(BaseDatatableView):
                 # item.course,
                 item.get_course_display(),
                 str(item.pk),
-                #reverse_lazy('student_home',kwargs={'pk': str(item.pk)})
-                reverse_lazy('student_home'),
+                reverse_lazy('student_detail',kwargs={'pk': str(item.pk)})
+                #reverse_lazy('student_home'),
             ])
             # print(json_data)
         return json_data
